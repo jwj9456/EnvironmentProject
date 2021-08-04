744c1764d6ca53ce6e8752896fe1878c8daf8806
KamaTAEWOO <zszs1010@naver.com>
2021-08-04 10:59:41 +0900

    1. test commplete 2. Project Start

---
+++ environmentProject/src/main/java/com/environment/web/filter/CharacterEncodingFilter.java
@@ -0,0 +1,31 @@
+// jsp 한글을 위한 filter file
+
+package com.environment.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("*.jsp")
+public class CharacterEncodingFilter implements Filter {
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		arg0.setCharacterEncoding("UTF-8");
+		arg1.setContentType("text/html; charset=UTF-8");
+		arg2.doFilter(arg0, arg1);
+	}
+
+	@Override
+	public void destroy() {}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {}
+}
